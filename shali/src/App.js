f2094511595186fabbb6fe5c66ee5aab73ddc083
darius khayyam <dv.Khayyam@gmail.com>
2020-06-20 15:24:45 +0200

    started project

---
+++ shali/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom"
+
+class App extends React.Component{
+    render(){
+        return(
+            <Router>
+               git  
+            </Router>
+        )
+    }
+}
+
+export default App
